721de74318ad9c621d37635be2e10a24ce60d1e8
a38782615 <38782615@qq.com>
2026-03-21 16:53:42 +0800

    restore particle cue spec comments

--- Unity/Assets/Scripts/Game/ET/Code/ModelView/Client/Skill/Runtime/Cue/ParticleCueSpec.cs
+++ Unity/Assets/Scripts/Game/ET/Code/ModelView/Client/Skill/Runtime/Cue/ParticleCueSpec.cs
@@ -2,22 +2,46 @@ namespace ET.Client
 {
     /// <summary>
     /// 粒子特效 Cue Spec
+    /// 播放粒子特效
     /// </summary>
     [ComponentOf(typeof(GameplayCueSpec))]
     public partial class ParticleCueSpec : Entity, IAwake
     {
+        // ============ 动态数据 ============
+
+        /// <summary>
+        /// 粒子实体 Id
+        /// </summary>
         public int ParticleEntityId { get; set; }
 
+        /// <summary>
+        /// 位置来源类型
+        /// </summary>
         public PositionSourceType PositionSource { get; set; }
 
+        /// <summary>
+        /// 绑定点名称
+        /// </summary>
         public string ParticleBindingName { get; set; }
 
+        /// <summary>
+        /// 位置偏移
+        /// </summary>
         public UnityEngine.Vector3 ParticleOffset { get; set; }
 
+        /// <summary>
+        /// 缩放
+        /// </summary>
         public UnityEngine.Vector3 ParticleScale { get; set; }
 
+        /// <summary>
+        /// 是否附着到目标
+        /// </summary>
         public bool AttachToTarget { get; set; }
 
+        /// <summary>
+        /// 是否循环
+        /// </summary>
         public bool ParticleLoop { get; set; }
     }
 }
