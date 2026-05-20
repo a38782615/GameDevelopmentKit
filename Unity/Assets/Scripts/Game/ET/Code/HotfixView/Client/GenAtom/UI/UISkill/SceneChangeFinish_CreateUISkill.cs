3b4ebb86cda3669b3975218c1db89f25cec5098e
a38782615 <38782615@qq.com>
2026-05-20 21:47:57 +0800

    feat: 启动后默认打开 UIMain

--- Unity/Assets/Scripts/Game/ET/Code/HotfixView/Client/GenAtom/UI/UISkill/SceneChangeFinish_CreateUISkill.cs
+++ Unity/Assets/Scripts/Game/ET/Code/HotfixView/Client/GenAtom/UI/UISkill/SceneChangeFinish_CreateUISkill.cs
@@ -6,12 +6,21 @@ namespace ET.Client
     [Event(SceneType.Current)]
     public class SceneChangeFinish_CreateUISkill : AEvent<Scene, SceneChangeFinish>
     {
+        private const string UIMainAssetName = "Assets/Res/UI/UIForm/GenAtom/UIMain.prefab";
+        private const string DefaultUIGroupName = "Default";
+
         protected override async UniTask Run(Scene scene, SceneChangeFinish args)
         {
             UIComponent uiComponent = scene.GetComponent<UIComponent>();
             uiComponent.RemoveComponent<UIFormSkillComponent>();
+            uiComponent.RemoveComponent<UIFormMainComponent>();
 
-            await scene.GetComponent<UIComponent>().AddUIFormComponentAsync<UIFormSkillComponent>(UGFUIFormId.UISkill);
+            UIFormMainComponent uiFormMain = uiComponent.AddComponent<UIFormMainComponent>();
+            await uiFormMain.OpenUIFormAsync(
+                UIMainAssetName,
+                DefaultUIGroupName,
+                Constant.AssetPriority.UIFormAsset,
+                false);
         }
     }
 }
