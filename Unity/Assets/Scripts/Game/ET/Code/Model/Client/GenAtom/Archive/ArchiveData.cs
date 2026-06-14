8b31832018202a526cd554ce1bdf916e47c3465f
a38782615 <38782615@qq.com>
2026-06-14 21:49:10 +0800

    Add Luban XRoot enum

---
+++ Unity/Assets/Scripts/Game/ET/Code/Model/Client/GenAtom/Archive/ArchiveData.cs
@@ -0,0 +1,13 @@
+
+namespace ET.Client
+{
+    public partial class PlayerData : Object
+    {
+        public int Age; //年龄
+        public int Exp; //经验
+        public int Level; //等级
+        public string NickName; //昵称
+        public int Diamond; //灵石
+        public ET.XRoot XRoot; // 灵根
+    }
+}
