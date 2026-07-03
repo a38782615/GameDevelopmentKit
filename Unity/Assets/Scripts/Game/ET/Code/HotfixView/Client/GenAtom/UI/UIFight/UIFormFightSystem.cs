c4a284cd07ff5d6ef8ba93e45fad6fd1ebc9756b
a38782615 <38782615@qq.com>
2026-07-03 13:48:00 +0800

    添加战斗地图切换事件

--- Unity/Assets/Scripts/Game/ET/Code/HotfixView/Client/GenAtom/UI/UIFight/UIFormFightSystem.cs
+++ Unity/Assets/Scripts/Game/ET/Code/HotfixView/Client/GenAtom/UI/UIFight/UIFormFightSystem.cs
@@ -1,4 +1,5 @@
 using Cysharp.Threading.Tasks;
+using Game;
 using UnityEngine;
 
 namespace ET.Client
@@ -12,6 +13,7 @@ namespace ET.Client
         private static void UGFUIFormOnOpen(this UIFormFight self)
         {
             self.OpenAllUIWidgets();
+            self.BindMapSwitchButtons();
             FightComponent fightComponent = self.Root().CurrentScene()?.GetComponent<FightComponent>();
             self.RefreshMapSwitchButtons(fightComponent);
             if (fightComponent == null)
@@ -25,6 +27,57 @@ namespace ET.Client
         [UGFUIFormSystem]
         private static void UGFUIFormOnClose(this UIFormFight self, bool isShutdown)
         {
+            self.UnbindMapSwitchButtons();
+            self.RemoveCurrentMapWidget();
+            self.IsSwitchingMap = false;
+        }
+
+        private static void BindMapSwitchButtons(this UIFormFight self)
+        {
+            self.View?.PreExButton?.SetAsync(async () => await self.SwitchMapAsync(-1));
+            self.View?.NextExButton?.SetAsync(async () => await self.SwitchMapAsync(1));
+        }
+
+        private static void UnbindMapSwitchButtons(this UIFormFight self)
+        {
+            self.View?.PreExButton?.onClick.RemoveAllListeners();
+            self.View?.NextExButton?.onClick.RemoveAllListeners();
+        }
+
+        private static async UniTask SwitchMapAsync(this UIFormFight self, int delta)
+        {
+            if (self.IsSwitchingMap)
+            {
+                return;
+            }
+
+            FightComponent fightComponent = self.Root().CurrentScene()?.GetComponent<FightComponent>();
+            if (fightComponent == null || self.Maps == null || self.Maps.Length == 0)
+            {
+                self.RefreshMapSwitchButtons(fightComponent);
+                return;
+            }
+
+            int nextMap = Mathf.Clamp(fightComponent.CurrentMap + delta, 0, self.Maps.Length - 1);
+            if (nextMap == fightComponent.CurrentMap)
+            {
+                self.RefreshMapSwitchButtons(fightComponent);
+                return;
+            }
+
+            self.IsSwitchingMap = true;
+            try
+            {
+                fightComponent.CurrentMap = nextMap;
+                self.RefreshMapSwitchButtons(fightComponent);
+                self.RemoveCurrentMapWidget();
+                await self.LoadWidgetMapAsync(fightComponent, self.View.CenterRectTransform);
+            }
+            finally
+            {
+                self.IsSwitchingMap = false;
+                self.RefreshMapSwitchButtons(fightComponent);
+            }
         }
 
         private static void RefreshMapSwitchButtons(this UIFormFight self, FightComponent fightComponent)
@@ -45,10 +98,23 @@ namespace ET.Client
             }
 
             var uiWidget = await self.LoadChildUIWidgetAsync<UIWidgetMap>(self.Maps[fightComponent.CurrentMap]);
+            self.CurrentMapWidget = uiWidget;
             uiWidget.CachedRectTransform.SetParent(parentRectTransform);
             uiWidget.CachedRectTransform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
             uiWidget.CachedRectTransform.localScale = Vector3.one;
             uiWidget.Open();
+        }
+
+        private static void RemoveCurrentMapWidget(this UIFormFight self)
+        {
+            UIWidgetMap currentMapWidget = self.CurrentMapWidget.As();
+            self.CurrentMapWidget = default;
+            if (currentMapWidget == null || currentMapWidget.IsDisposed)
+            {
+                return;
+            }
+
+            currentMapWidget.Dispose();
         }
     }
 }
--- Unity/Assets/Scripts/Game/ET/Code/ModelView/Client/GenAtom/UI/UIFight/UIFormFight.cs
+++ Unity/Assets/Scripts/Game/ET/Code/ModelView/Client/GenAtom/UI/UIFight/UIFormFight.cs
@@ -7,8 +7,10 @@ namespace ET.Client
     public class UIFormFight : UGFUIForm<MonoUIFormFight>, IAwake, IDestroy, IUGFUIFormOnOpen, IUGFUIFormOnClose
     {
         public int[] Maps = new int[3] { UGFUIEntityId.Map0, UGFUIEntityId.Map1 , UGFUIEntityId.Map2};
+        public EntityRef<UIWidgetMap> CurrentMapWidget;
         public readonly List<long> FightUnitIds = new List<long>();
         public bool IsLoadingFightUnits;
+        public bool IsSwitchingMap;
         public RectTransform[] LPos;
         public RectTransform[] RPos;
     }
